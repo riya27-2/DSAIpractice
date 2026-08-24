022af0fd67befa6af620c0cb6b2b8477f66e9c44
Riya Patel <riyapatel7228@gmail.com>
2026-08-24 22:24:20 +0530

    practise que

---
+++ Array/prac2.java
@@ -0,0 +1,16 @@
+public class prac2 {
+    public static void main(String[] args) {
+         int sum=0;
+
+        int matrix[][]={{2,4,5},{11,4,3},{2,1,4}};
+       
+             for(int j=0;j<matrix[0].length;j++){
+           sum += matrix[1][j];
+            }
+         System.out.println(sum);
+
+        }
+           
+            
+        
+}
