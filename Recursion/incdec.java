4207a239246330b73e08048721aa4809a898e5b5
Riya Patel <riyapatel7228@gmail.com>
2026-09-02 23:22:12 +0530

    increase & decrease numbers

--- Recursion/incdec.java
+++ Recursion/incdec.java
@@ -1,14 +1,28 @@
 public class incdec{
     public static void printDec(int n){
         if(n ==1){
-            System.out.println(1);
+            System.out.println(1+" ");
             return;
         }
         System.out.print(n +" ");
         printDec(n-1);
     }
+
+
+    public static void printInc(int n) {
+        if(n == 1){
+            System.out.print(1+" ");
+            return;
+        }
+
+         printInc(n-1);
+         System.out.print(n +" ");
+    }
+
+    
     public static void main(String args[]){
         int n=10;
+        printInc(n);
         printDec(n);
     }
 }
