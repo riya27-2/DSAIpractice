b3d373753fef208d9aeaa5dd19d8891add7cc728
Riya Patel <riyapatel7228@gmail.com>
2026-09-04 23:35:12 +0530

    no consequetive 1s

---
+++ Recursion/Binary.java
@@ -0,0 +1,22 @@
+public class Binary {
+
+    public static void binarystr(int n ,int lastplace,String str){
+
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+      binarystr(n-1,0,str+"0");
+
+            if( lastplace == 0){
+                binarystr(n-1,1,str+"1");
+
+            }
+
+            
+    }
+    public static void main(String[] args) {
+        
+        binarystr(3,0,"");
+    }
+}
