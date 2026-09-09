c3f23710da832ac5e6bdc8443adc8449a1b6da4d
Riya Patel <riyapatel7228@gmail.com>
2026-09-09 17:27:42 +0530

    added Merge sort

---
+++ DivideConquer/Merge.java
@@ -0,0 +1,62 @@
+
+public class Merge {
+
+    public static void printArr(int arr[]) {
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+ " ");
+        }
+        
+     }
+    public static void MergeSort(int arr[], int si, int ei){
+        if(si>=ei){
+            return;
+        }
+
+        int mid=si+(ei-si)/2;
+        MergeSort(arr, si,mid);
+        MergeSort(arr, mid+1, ei);
+        Merge(arr,si, mid,ei);
+
+    }
+    public static void Merge(int arr[], int si, int mid, int ei){
+        int temp[]=new int[ei-si+1];
+        int i=si;
+        int j=mid+1;
+        int k=0;
+        while(i <= mid && j<= ei){
+            if(arr[i]<arr[j]){
+                temp[k]=arr[i];
+                i++;
+                
+            }else{
+                temp[k]=arr[j];
+                j++;
+                
+            }
+            k++;
+        }
+
+        //left part
+
+        while(i<=mid){
+            temp[k++] = arr[i++];
+        }
+
+        //right part
+        while(j<=ei){
+            temp[k++] = arr[j++];
+        }
+
+        //copy temp to original array
+
+        for(k=0,i=si; k<temp.length; k++, i++){
+        arr[i]=temp[k];
+        }
+
+    }
+    public static void main(String[] args) {
+        int arr[]={6,7,1,9,0,2,4,5};
+        MergeSort(arr,0,arr.length-1);
+        printArr(arr);
+    }
+}
