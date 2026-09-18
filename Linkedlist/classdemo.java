81af33fd6674a231b6cd1959e28c9610038c43a8
Riya Patel <riyapatel7228@gmail.com>
2026-09-18 20:04:04 +0530

    remove last in LL

--- Linkedlist/classdemo.java
+++ Linkedlist/classdemo.java
@@ -1,3 +1,5 @@
+import org.w3c.dom.Node;
+
 public class classdemo {
 
     public static class Node {
@@ -99,6 +101,30 @@ public class classdemo {
         size--;
         return val;
     }
+
+    public int removeLast(){
+        if(size == 0){
+            System.out.println("LL is empty");
+            return Integer.MIN_VALUE;
+        }else if(size == 1){
+            int val= head.data;
+            head=tail=null;
+            size=0;
+            return val;
+        }
+        //prev = i=size-2
+        Node prev =head;
+        for(int i=0;i<size-2;i++){
+            prev=prev.next;
+        }
+        int val=prev.next.data;
+        prev.next=null;
+        tail=prev;
+        size--;
+        return val;
+
+
+    }
     public static void main(String[] args) {
 
         classdemo ll = new classdemo();
@@ -114,9 +140,12 @@ public class classdemo {
 
         ll.add(2,3);
         ll.print();
+
+        ll.removeLast();
+        ll.print();
         System.out.println(ll.size);
 
-        ll.removeFirst();
-        ll.print();
+
+
     }
 }
