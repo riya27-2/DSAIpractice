21275741862aedb12231368fcb48ea7a2dec57ad
Riya Patel <riyapatel7228@gmail.com>
2026-09-18 19:38:13 +0530

    add element in middle

--- Linkedlist/classdemo.java
+++ Linkedlist/classdemo.java
@@ -12,11 +12,13 @@ public class classdemo {
 
     public static Node head;
     public static Node tail;
+    public static int size;
 
     public void addFirst(int data) {
 
         // Step 1: Create new node
         Node newNode = new Node(data);
+        size++;
 
         if (head == null) {
             head = tail = newNode;
@@ -34,6 +36,7 @@ public class classdemo {
 
         // Create new node
         Node newNode = new Node(data);
+        size++;
 
         if (head == null) {
             head = tail = newNode;
@@ -61,25 +64,40 @@ public class classdemo {
         System.out.println("null");
     }
 
+    public void add(int idx, int data){
+
+        if(idx == 0){
+            addFirst(data);
+            return;
+        }
+        Node newNode=new Node(data);
+        size++;
+        Node temp=head;
+        int i=0;
+
+        while(i < idx-1){
+            temp =temp.next;
+            i++;
+        }
+
+        newNode.next = temp.next;
+        temp.next=newNode;
+    }
     public static void main(String[] args) {
 
         classdemo ll = new classdemo();
 
-        ll.print();
 
         ll.addFirst(2);
-        ll.print();
 
         ll.addFirst(1);
-        ll.print();
-
-        ll.addLast(3);
-        ll.print();
 
         ll.addLast(4);
-        ll.print();
 
-        ll.addFirst(5);
+        ll.addLast(5);
+
+        ll.add(2,3);
         ll.print();
+        System.out.println(ll.size);
     }
 }
